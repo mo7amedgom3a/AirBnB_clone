6e28a0803a3625870203d70d5ebbea79fbfd60f0
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-11-12 19:26:02 +0200

    test

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,7 @@ import os
 
 
 class FileStorage:
+
     """Class for storing and retrieving data"""
     __file_path = "file.json"
     __objects = {}
@@ -29,9 +30,19 @@ class FileStorage:
         """Returns a dictionary of valid classes and their references"""
         from models.base_model import BaseModel
         from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
 
         classes = {"BaseModel": BaseModel,
-                   "User": User}
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
         return classes
 
     def reload(self):
@@ -57,5 +68,28 @@ class FileStorage:
                       "password": str,
                       "first_name": str,
                       "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
         }
         return attributes

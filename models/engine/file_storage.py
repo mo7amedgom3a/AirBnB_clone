e705970cca5c58f867b2826a1ab1ae72d06201ae
mohamed gomaa <mo7amed.gom3a.7moda@gmail.com>
2023-11-12 16:14:05 +0200

    test

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,4 +32,7 @@ class FileStorage:
             return
         with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
             obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]](**v)
+                        for k, v in obj_dict.items()}
+            # TODO: should this overwrite or insert?
             FileStorage.__objects = obj_dict
